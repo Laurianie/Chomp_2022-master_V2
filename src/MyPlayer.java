48f3f3bf4dc23c9dd7072d347ce4ed5fed053955
laurianie guiteau <laurianie_guiteau24@milton.edu>
2022-12-01 10:44:32 -0500

    I need to print the lose pboards now, but I have already made my Array Lists.

--- src/MyPlayer.java
+++ src/MyPlayer.java
@@ -4,25 +4,29 @@ import java.util.ArrayList;
 public class MyPlayer {
     public Chip[][] gameBoard;
     public int[] columns;
+    ArrayList<int[]> allBoards = new ArrayList<int[]>();
+    ArrayList<int[]> loseBoards = new ArrayList<int[]>();
+
+
+
+
 
 
     public MyPlayer() {
         columns = new int[10];
-        ArrayList<int[]> allBoards = new ArrayList<>();
         for (int f = 1; f <= 3; f++) {
             for (int s = 0; s <= f; s++) {
                 for (int t = 0; t <= s; t++) {
                     System.out.println("board: " + f + "" + s + "" + t);
-                    generatePossibleBoards(f,s,t);
-
+                    generatePossibleBoards(f, s, t); //create board object
+                    int[] OGboards = new int[3];
+                    OGboards[0] = f;
+                    OGboards[1] = s;
+                    OGboards[2] = t;
+                    allBoards.add(OGboards);
                 }
             }
         }
-
-        /***
-         * This code will run just once, when the game opens.
-         * Add your code here.
-         */
     }
 
     public void generatePossibleBoards(int f, int s, int t) {
@@ -32,18 +36,26 @@ public class MyPlayer {
         int z = t;
         // x,y, and z are storing the original values of f,s,and t so that when boards like 111 and 222 need to be printed, we can refer to original values of
         //f,s,and t rather than the the changed values of f,s,and t
+
         for (int c = t-1; c >= 0; c--){
             System.out.println(f+""+s+""+c);
+            if (c == 0 && c == s && f == c+1){
+                System.out.println("LOSE^");
+            }
         }
         for (int b = s-1; b >=0; b--){
             if (b < t){
                 t = b;
             }
             System.out.println(f+""+b+""+t);
+            if (b == 0 && b == t && f == b+1){
+                System.out.println("LOSE^");
+            }
         }
         t = z;
         // f-1 (same for s-1 and t-1) allow the actual board not to be reprinted because the "-1" subtracts that number (3,2, or 1)
         // after all of the boards requiring that number have already been printed
+
         for (int a = f-1; a >= 1; a--){
             if (a < s){
                 s = a;
@@ -52,6 +64,21 @@ public class MyPlayer {
                 t = a;
             }
             System.out.println(a+""+s+""+t);
+            if (a == 1 && a == s+1 && a == t+1){
+                System.out.println("LOSE^");
+            }
+        }
+        int[] LB = new int[5];
+        LB[0] = 100;
+        LB[1] = 210;
+        LB[2] = 221;
+        LB[3] = 311;
+        LB[4] = 320;
+        loseBoards.add(LB);
+
+        for (int cycle = 0; cycle <= LB.length - 1; cycle++){
+            System.out.print("This is a losing board: ");
+            System.out.println(LB[cycle]);
         }
 
     }
